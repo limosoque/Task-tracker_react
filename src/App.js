7d445e4f012706027bdae76cb4133f908de5dbb3
limosoque <106828150+limosoque@users.noreply.github.com>
2024-03-21 20:49:27 +0300

    task's status changing added

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
         task => changedTask.id === task.id ? changedTask : task
       )
     }));
-  }
+  };
 
   const deleteTask = (deletedTask, status) => {
     setTasks(oldTasks => ({
@@ -65,19 +65,57 @@ function App() {
       ...oldTasks,
       [status]: oldTasks[status].filter(task => deletedTask.id !== task.id)
     }));
+  };
+
+  const changeTaskStatus = (changedTask, oldStatus, newStatus) => {
+    setTasks(oldTasks => {
+      const changedTasks = { ...oldTasks };
+
+      /* Находим задачу для изменения статуса */
+      const taskToChange = oldTasks[oldStatus].find(task => task.id === changedTask.id);
+
+      /* Меняем ей статус */
+      changedTasks[newStatus] = [...changedTasks[newStatus], taskToChange];
+
+      /* Удаляем задачу из старого статуса */
+      changedTasks[oldStatus] = changedTasks[oldStatus].filter(task => task.id !== changedTask.id);
+
+      return changedTasks;
+    })
   }
 
   /* Создание статусов-столбцов */
   return (
     <div className='taskboard'>
-      <Status title="Todo" tasks={tasks.todo} addTask={addTask} changeTask={changeTask} deleteTask={deleteTask} />
-      <Status title="In progress" tasks={tasks.inProgress} addTask={addTask} changeTask={changeTask} deleteTask={deleteTask} />
-      <Status title="Done" tasks={tasks.done} addTask={addTask} changeTask={changeTask} deleteTask={deleteTask} />
+      <Status 
+        title="Todo" 
+        tasks={tasks.todo} 
+        addTask={addTask} 
+        changeTask={changeTask} 
+        deleteTask={deleteTask} 
+        changeTaskStatus={changeTaskStatus} 
+      />
+      <Status 
+        title="In progress" 
+        tasks={tasks.inProgress} 
+        addTask={addTask} 
+        changeTask={changeTask} 
+        deleteTask={deleteTask} 
+        changeTaskStatus={changeTaskStatus} 
+      />
+      <Status 
+        title="Done" 
+        tasks={tasks.done} 
+        addTask={addTask} 
+        changeTask={changeTask} 
+        deleteTask={deleteTask} 
+        changeTaskStatus={changeTaskStatus} 
+      />
     </div>
   );
 }
 
-function Status({ title, tasks, addTask, changeTask, deleteTask }) {
+function Status({ title, tasks, addTask, changeTask, deleteTask, changeTaskStatus }) {
   let taskStatus = 'todo';
   if (title === "In progress") {
     taskStatus = 'inProgress';
@@ -107,6 +145,10 @@ function Status({ title, tasks, addTask, changeTask, deleteTask }) {
     deleteTask(task, taskStatus);
   }
 
+  const handleChangeTaskStatus = (task, oldStatus, newStatus) => {
+    changeTaskStatus(task, oldStatus, newStatus);
+  }
+
   return (
     <div className='status'>
       <div className='statusAndAddTaskButton'>
@@ -119,20 +161,34 @@ function Status({ title, tasks, addTask, changeTask, deleteTask }) {
         <Task
           key={task.id}
           task={task}
+          taskStatus={taskStatus}
           setEditingTask={setEditingTask}
           /* Сообщаем, редактируется ли задача */
           isEditing={task === editingTask}
           onSave={handleSaveTask}
           onDelete={handleDeleteTask}
+          onChangeTaskStatus={handleChangeTaskStatus}
         />
       ))}
     </div>
   );
 }
 
-function Task({ task, setEditingTask, isEditing, onSave, onDelete }) {
+function Task({ task, taskStatus, setEditingTask, isEditing, onSave, onDelete, onChangeTaskStatus }) {
   /* Запоминаем изменение задачи */
   const [editedTask, setEditedTask] = useState(task);
+
+  /* Определяем, куда можно переместить текущую задачу */
+  let alternativeTaskStatuses = [];
+  if(taskStatus === 'todo'){
+    alternativeTaskStatuses = ['inProgress', 'done'];
+  }
+  else if (taskStatus === 'inProgress'){
+    alternativeTaskStatuses = ['todo', 'done'];
+  }
+  else{
+    alternativeTaskStatuses = ['todo', 'inProgress'];
+  }
 
   /* Изменение задачи */
   const handleTaskChange = (event) => {
@@ -150,6 +206,10 @@ function Task({ task, setEditingTask, isEditing, onSave, onDelete }) {
 
   const handleDeleteTask = () => {
     onDelete(task);
+  }
+
+  const changeTaskStatus = (newStatus) => {
+    onChangeTaskStatus(task, taskStatus, newStatus);
   }
 
   if (isEditing) {
@@ -178,9 +238,13 @@ function Task({ task, setEditingTask, isEditing, onSave, onDelete }) {
     <div className='task' onDoubleClick={handleDoubleClick}>
       <div className='taskTitleAndDeleteButton'>
         <h3 className='text_wrapped'>{task.title}</h3>
-        <button className='button' onClick={() => handleDeleteTask()}>Delete</button>
+        <button className='delete_button' onClick={() => handleDeleteTask()}>Delete</button>
       </div>
       <p className="text_wrapped">{task.task}</p>
+      <div className='taskTitleAndDeleteButton'>
+        <button className='button' onClick={() => changeTaskStatus(alternativeTaskStatuses[0])}>{alternativeTaskStatuses[0]}</button>
+        <button className='button' onClick={() => changeTaskStatus(alternativeTaskStatuses[1])}>{alternativeTaskStatuses[1]}</button>
+      </div>
     </div>
   );
 }
